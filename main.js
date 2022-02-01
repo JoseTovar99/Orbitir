ed8e666776827f21feceb750072e5b2f5678dabc
JoseTovar510 <tovarjose1013@gmail.com>
2022-01-31 20:56:17 -0800

    More style changes with innerHTML

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ let btn2 = document.querySelector('#marginLow2');
 let diffTitle = document.querySelector('#titleChange');
 
 btn1.addEventListener('click', () => {
-    img.src = 'img/MarsTest.JPG'
+    img.src = 'img/MarsTest.JPG';
+    document.getElementById("titleChange").innerHTML = "MARS";
+    document.getElementById("milesChange").innerHTML = "228.69 million miles";
+
+
 })
 btn2.addEventListener('click', () => {
     img.src = 'img/Neptune3.jpg';
     document.getElementById("titleChange").innerHTML = "NEPTUNE";
+    document.getElementById("milesChange").innerHTML = "2.8508 billion miles";
+
 
 })
 
