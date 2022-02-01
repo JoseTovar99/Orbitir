64d132a2e9b43b5210d6259844ab4a3f9d7896fa
JoseTovar510 <tovarjose1013@gmail.com>
2022-01-31 20:37:33 -0800

    Adding JS onclick events

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const hamburger = document.getElementById('hamburger'); 
 const navTitle = document.getElementById('navTitle');
 
+let img = document.querySelector('#marsID');
+let btn1 = document.querySelector('#marginLow');
+let btn2 = document.querySelector('#marginLow2');
+let diffTitle = document.querySelector('#titleChange');
+
+btn1.addEventListener('click', () => {
+    img.src = 'img/MarsTest.JPG'
+})
+btn2.addEventListener('click', () => {
+    img.src = 'img/Neptune3.jpg';
+    document.getElementById("titleChange").innerHTML = "NEPTUNE";
+
+})
+
+
+
+
 hamburger.addEventListener('click', () => {
     navTitle.classList.toggle('show');
 })
+
+
